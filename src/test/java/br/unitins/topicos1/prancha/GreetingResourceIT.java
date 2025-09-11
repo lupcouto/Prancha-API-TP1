15abd0c8d8057546e2db6dc5c57ddd62c4c7e505
Julia Couto <juliapcouto18@gmail.com>
2025-09-11 16:36:18 -0300

    CRUD inicial de Usuario

---
+++ src/test/java/br/unitins/topicos1/prancha/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package br.unitins.topicos1.prancha;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
